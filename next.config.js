b3b9abf99b1c61c744935e769d4de0267dfa7b28
arsnonverba <www@arsnonverba.org>
2022-11-06 20:25:52 -0600

    updated config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
+const path = await import('path');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  images: {
+    disableStaticImages: true,
+    domains: ['arweave.net', 'metamural.io', 'metmuralnfts.s3.us-east-2.amazonaws.com', 'lh3.googleusercontent.com']
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/assets/scss')],
+  },
+}
