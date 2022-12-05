a8377a63803f21a504d5753b77e966635cb9c816
arsnonverba <www@arsnonverba.org>
2022-12-04 19:41:29 -0600

    add post list component

--- components/shared/post-list.tsx
+++ components/shared/post-list.tsx
@@ -7,7 +7,7 @@ import Marquee from 'react-fast-marquee';
 export default function post() {
   return (
     <section
-      id="main-post"
+      id="main-layout-post"
       className={cx(
         "relative flex place-content-center",
         "w-[100vw] h-[100vh] sm:h-[40rem] md:h-[100vh]",
@@ -17,75 +17,77 @@ export default function post() {
       <div
         id="post"
         className={cx(
-          "relative flex flex-col m-auto",
+          "group cursor-pointer relative flex flex-col m-auto",
           "w-[40rem] md:w-[50rem] lg:w-[60rem] 2xl:w-[70rem] 4xl:w-[80rem]",
           "h-[30rem] md:h-[40rem] lg:h-[40rem] 2xl:h-[50rem] 4xl:h-[60rem]",
           "bg-bg",
+          "lg:hover:scale-105 transition duration-200 ease-in-out",
         )}
       >
-      <div
-        id="content-image"
-        className={cx(
-          "relative",
-          "w-[100%] h-[100%]",
-          "gradient-primary",
-        )}
-      >
-        <Image
+        <div
+          id="content-image"
+          className={cx(
+            "relative",
+            "w-[100%] h-[100%]",
+            "gradient-primary",
+          )}
+        >
+          <Image
             src="/beauty.jpg"
             fill
             sizes="100%"
             alt="postimage"
             className="z-10 object-cover"
-        />
+          />
+          <div
+            id="content-image-shadow"
+            className={cx(
+              "absolute -z-0",
+              "w-[100%] h-[100%] md:ml-[1rem] mt-[1rem]",
+              "gradient-primary blur-md group-hover:gradient-secondary",
+            )}
+          >
+          </div>
+          <div
+            id="content-image-title"
+            className={cx(
+              "absolute z-20 flex bottom-0 place-content-end",
+              "w-[100%] h-[10vh]",
+              "bg-transparent font-basier text-2xl text-uppercase",
+            )}
+          >
+            <div className="flex-row w-[100%] h-[3rem] bg-bg rounded-sm hidden lg:inline-flex">
+              <div className="bg-black p-[.5rem] text-bg  h-[100%] w-[100%]">
+                andrea zuckerman.
+              </div>
+            </div>
+            <div className="flex flex-row w-[100%] h-[3rem] bg-bg rounded-sm lg:hidden">
+              <div className="bg-black p-[.5rem] text-bg h-[100%] w-[100%]">
+              <Marquee gradient={false} delay={3} speed={62} loop={5} pauseOnHover={true}>
+                andrea zuckerman.
+              </Marquee>
+              </div>
+            </div>
+          </div>
+        </div>
         <div
-          id="content-image-shadow"
+          id="content-post"
           className={cx(
-            "absolute -z-0",
-            "w-[100%] h-[100%] md:ml-[1rem] mt-[1rem]",
-            "gradient-primary blur-md",
-          )}
-        />
-        <div
-          id="content-image-title"
-          className={cx(
-            "absolute z-20 flex bottom-0 place-content-end",
-            "w-[100%] h-[10vh]",
-            "bg-transparent font-basier text-2xl text-uppercase",
+            "relative flex flex-col gap-[1rem]",
+            "mt-[3rem] mx-[.5rem]",
+            "bg-bg font-basier",
           )}
         >
-          <div className="flex-row w-[100%] h-[3rem] bg-bg rounded-sm hidden lg:inline-flex">
-            <div className="bg-black p-[.5rem] text-bg h-[100%] w-[100%]">
-              andrea zuckerman.
-            </div>
+          <div id="content-post-tags" className="font-bold text-primary text-sm text-uppercase">
+            activismo, feminismo, riot girl
           </div>
-          <div className="flex flex-row w-[100%] h-[3rem] bg-bg rounded-sm lg:hidden">
-            <div className="bg-black p-[.5rem] text-bg h-[100%] w-[100%]">
-            <Marquee gradient={false} delay={3} speed={62} loop={5} pauseOnHover={true}>
-              andrea zuckerman.
-            </Marquee>
-            </div>
+          <div className="group-hover:underline">
+            <p>
+              Un grupo de amigas se reunieron para hacer un divertido STORY de moda que reflejara su amistad en una secuencia divertida, dulce y creativa.
+            </p>
           </div>
         </div>
       </div>
-      <div
-        id="content-post"
-        className={cx(
-          "relative flex flex-col gap-[1rem]",
-          "mt-[3rem] mx-[.5rem]",
-          "bg-bg font-basier",
-        )}
-      >
-        <div id="content-post-tags" className="font-bold text-primary text-sm text-uppercase">
-          activismo, feminismo, riot girl
-        </div>
-        <div>
-          <p>
-            Un grupo de amigas se reunieron para hacer un divertido STORY de moda que reflejara su amistad en una secuencia divertida, dulce y creativa.
-          </p>
-        </div>
-      </div>
-    </div>
     </section>
   );
 };
